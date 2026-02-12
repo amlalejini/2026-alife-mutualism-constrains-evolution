fdb04c9a419e0ec53fe16826d5183d81405769b9
Delaney Kelley <kelleyde@mail.gvsu.edu>
2026-02-12 15:00:02 -0500

    update python and gcc

--- hpc-env/clipper-hpc-env.sh
+++ hpc-env/clipper-hpc-env.sh
@@ -1,9 +1,6 @@
-# PROJECT_NAME=Aagos
-# REPO_DIR=~/devo_ws/Aagos
-
 module purge
-module load python/3.11.9
-module load gcc/14.2.0
+module load python/3.14.0
+module load gcc/15.2.0
 module load cmake
 
 # source ${REPO_DIR}/pyenv/bin/activate
