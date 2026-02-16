3702974085b26c8af28678b9b28a21c127f941d5
Delaney Kelley <kelleyde@mail.gvsu.edu>
2026-02-15 21:31:51 -0500

    add RUN_PARAMS

--- experiments/2026-02-12-health-mut-rate/hpc/agg-slurm.sh
+++ experiments/2026-02-12-health-mut-rate/hpc/agg-slurm.sh
@@ -13,12 +13,8 @@ REPO_DIR=/mnt/home/kelleyde/research/${PROJECT_NAME}
 source ${HPC_ENV_FILEPATH}
 source ${REPO_DIR}/pyenv/bin/activate
 
-DATA_DIR=/mnt/scratch/lalejina_scratch/kelleyde/data/2025-fall-gvsu-symbiosis-mechanisms/2026-02-12-health-mut-rate/
-DUMP_DIR=/research/2025-fall-gvsu-symbiosis-mechanisms/experiments/2026-02-12-health-mut-rate/
-SUMMARY_UPDATE=500000
+RUN_PARAMS="--data_dir /mnt/scratch/lalejina_scratch/kelleyde/data/2025-fall-gvsu-symbiosis-mechanisms/2026-02-12-health-mut-rate/ --dump_dir /research/2025-fall-gvsu-symbiosis-mechanisms/experiments/2026-02-12-health-mut-rate/ --summary_update 500000"
 
-srun python3 ${REPO_DIR}/experiments/2026-02-12-health-mut-rate/analysis/aggregate.py \
-  --data_dir ${DATA_DIR} \
-  --dump_dir ${DUMP_DIR} \
-  --summary_update ${SUMMARY_UPDATE}
+srun python3 ${REPO_DIR}/experiments/2026-02-12-health-mut-rate/analysis/aggregate.py ${RUN_PARAMS}
+
  
